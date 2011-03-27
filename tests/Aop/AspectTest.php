69ecb6bb74f971790b3fad9ebc9b2e75a0d53706
max <max@ubuntu.localdomain>
2011-03-27 03:39:20 -0700

    freeze pointcut when registered with aspect

--- tests/Aop/AspectTest.php
+++ tests/Aop/AspectTest.php
@@ -9,11 +9,24 @@ class AspectTest extends PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->reflectionClassMock = $this->getMock('ReflectionClass', array(), array(), '', false);
+
+        $this->beforePointcutMock_1 = $this->getMock('Aop\Pointcut', array(), array(), '', false);
+        $this->beforePointcutMock_2 = $this->getMock('Aop\Pointcut', array(), array(), '', false);
+
+        $this->afterPointcutMock_1 = $this->getMock('Aop\Pointcut', array(), array(), '', false);
+        $this->afterPointcutMock_2 = $this->getMock('Aop\Pointcut', array(), array(), '', false);
     }
 
     public function testIsApplicableForReturnsFalseIfNoMatherExist()
     {
         $aspect = new Aspect();
         $this->assertFalse($aspect->isApplicableFor($this->reflectionClassMock));
+    }
+
+    public function testRegisteringPointcutFreezesPointcutInstance()
+    {
+        $aspect = new Aspect();
+        $this->beforePointcutMock_1->expects($this->once())->method('freeze');
+        $aspect->registerBeforePointcut($this->beforePointcutMock_1);
     }
 }
