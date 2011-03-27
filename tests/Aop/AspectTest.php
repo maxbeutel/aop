f91a537908a414c73b0da9636528b91eec0bf744
max <max@ubuntu.localdomain>
2011-03-27 03:56:48 -0700

    added tests for registering and execing pointcuts

--- tests/Aop/AspectTest.php
+++ tests/Aop/AspectTest.php
@@ -6,6 +6,16 @@ use PHPUnit_Framework_TestCase;
 
 class AspectTest extends PHPUnit_Framework_TestCase
 {
+    protected $reflectionClassMock;
+
+    protected $beforePointcutMock_1;
+    protected $beforePointcutMock_2;
+
+    protected $afterPointcutMock_1;
+    protected $afterPointcutMock_2;
+
+    protected $_argumentsMock;
+
     public function setUp()
     {
         $this->reflectionClassMock = $this->getMock('ReflectionClass', array(), array(), '', false);
@@ -15,6 +25,8 @@ class AspectTest extends PHPUnit_Framework_TestCase
 
         $this->afterPointcutMock_1 = $this->getMock('Aop\Pointcut', array(), array(), '', false);
         $this->afterPointcutMock_2 = $this->getMock('Aop\Pointcut', array(), array(), '', false);
+
+        $this->argumentsMock = $this->getMock('Aop\Pointcut\Arguments', array(), array(), '', false);
     }
 
     public function testIsApplicableForReturnsFalseIfNoMatherExist()
@@ -54,5 +66,25 @@ class AspectTest extends PHPUnit_Framework_TestCase
         $this->setExpectedException('InvalidArgumentException', 'Pointcut already registered');
 
         $aspect->registerAfterPointcut($this->afterPointcutMock_1);
+    }
+
+    public function testRegisterAndExecBeforePointcut()
+    {
+        $this->beforePointcutMock_1->expects($this->any())->method('getHashCode')->will($this->returnValue('123'));
+        $this->beforePointcutMock_1->expects($this->once())->method('exec');
+
+        $aspect = new Aspect();
+        $aspect->registerBeforePointcut($this->beforePointcutMock_1);
+        $aspect->execBeforePointcuts(array('123' => 0), $this->argumentsMock);
+    }
+
+    public function testRegisterAndExecAfterPointcut()
+    {
+        $this->afterPointcutMock_1->expects($this->any())->method('getHashCode')->will($this->returnValue('123'));
+        $this->afterPointcutMock_1->expects($this->once())->method('exec');
+
+        $aspect = new Aspect();
+        $aspect->registerAfterPointcut($this->afterPointcutMock_1);
+        $aspect->execAfterPointcuts(array('123' => 0), $this->argumentsMock);
     }
 }
